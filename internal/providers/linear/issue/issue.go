d4840bfef083e70e3e823ccd14234875bfb393bf
Oskar Hane <oh@oskarhane.com>
2026-09-09 17:18:22 +0200

    linear-pull-gaps/06-issue-comments-leaf: linear issue comments verb with reply threading

--- internal/providers/linear/issue/issue.go
+++ internal/providers/linear/issue/issue.go
@@ -20,5 +20,6 @@ func NewCmd(cfg *app.Config, newSvc service.Dialer[service.IssueService], viewer
 	cmd.AddCommand(newGetCmd(cfg, newSvc))
 	cmd.AddCommand(newCreateCmd(cfg, newSvc))
 	cmd.AddCommand(newUpdateCmd(cfg, newSvc))
+	cmd.AddCommand(newCommentsCmd(cfg, newSvc))
 	return cmd
 }
